62160d2602ca768712f12b920783aa681dd6a3a3
Rukshan shady <rukshan.shady@gmail.com>
2020-10-06 01:10:54 +0545

    heroku build

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  // app.use(
-  //   '/api',
-  //   createProxyMiddleware({
-  //     target: 'https://service-system-backend.herokuapp.com/',
-  //     changeOrigin: true,
-  //   })
-  // );
-  app.use(createProxyMiddleware("/api", { target: "https://service-system-backend.herokuapp.com" }));
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://service-system-backend.herokuapp.com/',
+      changeOrigin: true,
+      pathRewrite: {'^/api' : ''}
+    })
+  );
+  
 };
