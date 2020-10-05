d56a41669f908c7eeb8a6442b9d92703cfcf0c55
Rukshan shady <rukshan.shady@gmail.com>
2020-10-06 01:03:55 +0545

    heroku deploy cra

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://service-system-backend.herokuapp.com/',
-      //  changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   '/api',
+  //   createProxyMiddleware({
+  //     target: 'https://service-system-backend.herokuapp.com/',
+  //     changeOrigin: true,
+  //   })
+  // );
+  app.use(createProxyMiddleware("/api", { target: "https://service-system-backend.herokuapp.com" }));
 };
