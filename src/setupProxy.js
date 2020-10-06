79c95dec3832df3529bff9215b884d17aa0725ef
Roshan Pratap Katel <paradise98425@gmail.com>
2020-10-06 10:54:21 +0545

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,6 @@ module.exports = function(app) {
     createProxyMiddleware({
       target: 'https://service-system-backend.herokuapp.com/',
       changeOrigin: true,
-      pathRewrite: {'^/api' : ''}
     })
   );
   
